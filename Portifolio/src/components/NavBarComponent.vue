d31e66ca6b2d27f91c0845d92ca082eac3264846
takiguchi <alexandre.pupim360@gmail.com>
2025-05-21 21:29:18 -0300

    feat: Create Navbar/Footer

---
+++ Portifolio/src/components/NavBarComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <nav class="bg-blue-600 text-white py-4 px-8 flex justify-between items-center">
+        <h1 class="text-lg font-bold">Mini-Portifolio</h1>
+        <ul class="flex gap-6">
+            <li>
+                <router-link to="/" class="hover:underline">Quem Sou Eu? </router-link>
+            </li>
+            <li>
+                <router-link to=" " class="hover:underline">Meus projetos</router-link>
+            </li>
+            <li>
+                <router-link to="/contact" class="hover:underline">Contato</router-link>
+            </li>
+
+        </ul>
+    </nav>
+</template>
