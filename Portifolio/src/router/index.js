73ddbd95af616f2dc67a3089e0a7aa6e1019bb37
takiguchi <alexandre.pupim360@gmail.com>
2025-05-21 21:46:07 -0300

    feat: Create router

--- Portifolio/src/router/index.js
+++ Portifolio/src/router/index.js
@@ -1,23 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import AboutMeView from '../views/AboutMeView.vue'
+import ContactView from '../views/ContactView.vue'
+import MyprojectsView from '../views/MyprojectsView.vue'
 
-const router = createRouter({
+const routes = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       name: 'home',
-      component: HomeView,
+      component:AboutMeView
+    }
+    ,
+    {
+      path: 'contact',
+      name: 'contact',
+      component: ContactView
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-  ],
-})
+      path: 'projects',
+      name: 'projects',
+      component: MyprojectsView
+    }
 
-export default router
+  ],  
+})
