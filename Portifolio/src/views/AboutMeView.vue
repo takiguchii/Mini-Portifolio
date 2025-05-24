1a224c5d46c8732e5195f2969de78a06bf77714b
takiguchi <alexandre.pupim360@gmail.com>
2025-05-24 12:55:37 -0300

    feat: create card struct for about

--- Portifolio/src/views/AboutMeView.vue
+++ Portifolio/src/views/AboutMeView.vue
@@ -1,30 +1,46 @@
 <template>
   <div class="w-full max-w-2xl mx-auto mt-10 text-center">
-    <p class="text-lg font-medium mb-4">Algumas das minhas tecnologias</p>
+    <h1 class="text-6xl font-bold mb-8 leading-tight">
+      <span class="text-black hover:text-blue-600 transition-colors duration-300">MINHAS </span>
+      <span class="text-gray-600 hover:text-blue-400 transition-colors duration-300">TECNOLOGIAS</span>
+    </h1>
 
-    <!-- o card principal -->
+    <!-- o card Linguagem -->
+    <div @click="toggle" class="bg-gray-200 px-6 py-3 rounded-xl cursor-pointer hover:bg-gray-300 transition-colors">
+      Linguagens de Programação
+    </div>
+
+    <div v-if="isOpen" class="bg-gray-100 mt-3 px-5 py-3 rounded-lg text-sm text-gray-700">
+      <div
+        class="flex items-center space-x-4 bg-gray-200 px-6 py-3 rounded-xl cursor-pointer hover:bg-gray-300 transition-color">
+
+      </div>
+    </div>
+
+
+
+    <!-- o card framework -->
     <div @click="toggle" class="bg-gray-200 px-6 py-3 rounded-xl cursor-pointer hover:bg-gray-300 transition-colors">
       Frameworks
     </div>
-
-    <!-- Conteúdo expandido -->
     <div v-if="isOpen" class="bg-gray-100 mt-3 px-5 py-3 rounded-lg text-sm text-gray-700">
-      <div class="flex items-center space-x-4">
-        <img src="https://skillicons.dev/icons?i=cs" height="40" alt="csharp logo" />
-        <img width="12" />
-        <img src="https://skillicons.dev/icons?i=html" height="40" alt="html5 logo" />
-        <img width="12" />
-        <img src="https://skillicons.dev/icons?i=css" height="40" alt="css3 logo" />
-        <img width="12" />
-        <img src="https://skillicons.dev/icons?i=js" height="40" alt="javascript logo" />
-        <img width="12" />
-        <img src="https://skillicons.dev/icons?i=py" height="40" alt="python logo" />
-        <img width="12" />
-        <img src="https://skillicons.dev/icons?i=docker" height="40" alt="docker logo" />
-        <img width="12" />
-        <img src="https://skillicons.dev/icons?i=git" height="40" alt="git logo" />
+      <div class="flex items-center space-x-4 bg-gray-200 px-6 py-3 rounded-xl cursor-pointer hover:bg-gray-300 transition-color">     
+
       </div>
     </div>
+
+
+
+    <!-- o card ferramentas  -->
+    <div @click="toggle" class="bg-gray-200 px-6 py-3 rounded-xl cursor-pointer hover:bg-gray-300 transition-colors">
+      Ferramentas
+    </div>
+    <div v-if="isOpen" class="bg-gray-100 mt-3 px-5 py-3 rounded-lg text-sm text-gray-700">
+      <div class="flex items-center space-x-4 bg-gray-200 px-6 py-3 rounded-xl cursor-pointer hover:bg-gray-300 transition-color">
+
+      </div>
+    </div>
+
   </div>
 </template>
 
